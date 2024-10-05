56d53e8ec57e47ca5f0dbb9261308bd4bd47c505
gelo rosi <angelowither@gmail.com>
2024-10-05 15:46:32 +0800

    feat: add user model for user data

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,17 @@
-let users = [
-   
-];
+const fs = require('fs');
+const path = require('path');
+const usersFile = path.join(__dirname, '../data/users.json');
 
-module.exports = users;
+const getUsers = () => {
+    const usersData = fs.readFileSync(usersFile);
+    return JSON.parse(usersData);
+}
+
+const saveUsers = (users) => {
+    fs.writeFileSync(usersFile, JSON.stringify(users, null, 2));
+}
+
+module.exports = {
+    getUsers,
+    saveUsers
+};
