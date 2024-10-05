2503aaf80a2959f0c50ce3a13e7360db208f9b48
gelo rosi <angelowither@gmail.com>
2024-10-05 17:37:34 +0800

    fix: quick fix

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
+const loggerMiddleware = require('./middleware/loggerMiddleware');
 const rateLimiter = require('./middleware/limiterMiddleware');
 
 const app = express();
+
 app.use(bodyParser.json());
-
-app.use('/user', userRoutes);
-
+app.use(loggerMiddleware);
 app.use(rateLimiter);
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
+app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
