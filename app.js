569299865583cad4ad808f201d1699658fb6dc1e
gelo rosi <angelowither@gmail.com>
2024-10-05 15:36:58 +0800

    feat: add logger middleware

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const userRoutes = require('./routes/user');
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
 app.use(bodyParser.json());
 
 app.use('/user', userRoutes);
