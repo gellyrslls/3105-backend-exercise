196b71cff60a0a2ff49c04383dd85784776c4a86
gelo rosi <angelowither@gmail.com>
2024-10-05 17:15:28 +0800

    feat: add auth to show profile

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
+const rateLimiter = require('./middleware/limiterMiddleware');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.use('/user', userRoutes);
-app.use(bodyParser.json());
+
+app.use(rateLimiter);
 
 app.get('/', (req, res) => {
   res.send('Server is running');
--- controllers/userController.js
+++ controllers/userController.js
@@ -32,16 +32,32 @@ const login = (req, res) => {
     const user = users.find((u) => u.username === username && u.password === password);
   
     if (!user) {
-      return res.status(400).json({ message: 'Invalid username or password' });
+      return res.status(400).json({ message: 'Invalid username or password!' });
     }
   
     // simulate token generation 
     const token = `fake-token-${user.id}`;
   
-    return res.json({ message: 'Login successful', token });
+    return res.json({ message: 'Login successful!', token });
+  };
+
+  // shows user information
+  const getProfile = (req, res) => {
+    const token = req.headers['authorization'];
+    const userId = token.split('-').pop(); 
+  
+    const users = getUsers();
+    const user = users.find((u) => u.id === parseInt(userId));
+  
+    if (!user) {
+      return res.status(404).json({ message: 'User not found!' });
+    }
+  
+    return res.json({ username: user.username, email: user.email });
   };
 
 module.exports = { 
     register, 
-    login 
+    login,
+    getProfile
 };
