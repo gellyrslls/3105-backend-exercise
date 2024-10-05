297161a3a120d9d76bcb983d333904b4d089125a
gelo rosi <angelowither@gmail.com>
2024-10-05 15:52:41 +0800

    feat: init project structure

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,12 @@
-const users = require('../models/userModel'); 
+const userModel = require('../models/userModel'); 
 
-exports.registerUser = (req, res) => {
+exports.register = (req, res) => {
+    const { username, password, email } = req.body;
+    const users = userModel.getUsers();
 
+    if (users.find(user => user.username === username)) {
+        return res.status(400).json({ message: "User already exists" });
+    }
 };
 
 exports.loginUser = (req, res) => {
